2465cc98c4feff28cd07770d482a83465f46b561
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-12-09 20:42:07 -0300

    adc dados conta

--- system/pages/points.php
+++ system/pages/points.php
@@ -79,3 +79,20 @@ $(function() {
 	$('#account-name-input').focus();
 });
 </script>
+<div style="text-align:center">
+    <h1>Donate - OverOt Server</h1>
+    <h3>
+        Estamos aceitando apenas transferencia bancaria no momento.
+    </h3>
+</div>
+
+<ul>
+    <h2>Dados:</h2>
+    <li><b>Banco:</b> NuBank</li>
+    <li><b>Agencia:</b> 0001</li>
+    <li><b>C/C:</b> 24234677-9</li>
+    <li><b>Nome:</b> Andre Felipe Santos Cruz</li>
+    <li><b>CPF:</b> 405.336.638.06</li>
+</ul>
+
+<h3>Após concluir sua transferencia, encaminhe o comprovante para o email: <a href="mailto:overot@gmail.com">overot@gmail.com</a></h3>
